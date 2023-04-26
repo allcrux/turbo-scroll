a34894800cf8a2ef2da8598ace0eb9e91928dea9
eugen neagoe <eneagoe@gmail.com>
2023-04-26 17:13:50 +0300

    Make it work with view_compoennt 3.0+

--- lib/turbo-scroll/railtie.rb
+++ lib/turbo-scroll/railtie.rb
@@ -1,5 +1,17 @@
 module TurboScroll
   class Railtie < ::Rails::Railtie
+    config.before_initialize do
+      module ::ViewComponent
+        class Base < ActionView::Base
+          class << self
+            def config
+              @config ||= ViewComponent::Config.defaults
+            end
+          end
+        end
+      end
+    end
+
     initializer "turbo-scrolls.load_components" do
       ActiveSupport.on_load(:action_view) do
         require_relative "auto"
@@ -9,11 +21,12 @@ module TurboScroll
         require_relative "spin_loader"
       end
     end
+
     initializer "turbo-scrolls.view_helpers" do |app|
       ActiveSupport.on_load(:action_view) do
         include TurboScroll::ViewHelpers
       end
-      app.config.i18n.load_path += Dir[File.expand_path(File.join(File.dirname(__FILE__), '../locales', '*.yml')).to_s]
+      app.config.i18n.load_path += Dir[File.expand_path(File.join(File.dirname(__FILE__), "../locales", "*.yml")).to_s]
     end
   end
 end
